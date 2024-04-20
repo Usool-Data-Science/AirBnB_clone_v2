045ccecef38fe42a04d1b6f529fa162b79290dc0
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-04-20 03:13:34 +0100

    Solve task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Takes and return url arguments when visiting the route"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hbnb(text):
+    """Prints display C followed by the text argumente"""
+    return f"C {text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
